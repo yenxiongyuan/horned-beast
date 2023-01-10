d4321a6c35c23e030c81a13dd43f6ab428808cbe
Yen Xiong Yuan <yenyuan17@gmail.com>
2023-01-10 23:25:07 +0000

    fix img link

--- src/Main.js
+++ src/Main.js
@@ -5,15 +5,16 @@ class Main extends React.Component {
   render() {
     return (
       <>
-        <h2>
-          <HornedBeast title="UniWhal" />
-        </h2>
-        <p>
-          <HornedBeast description="A unicorn and a narwhal nuzzling their horns" />
-        </p>
-        <img src = {require(HornedBeast imageUrl="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" />)
-        alt="UniWhal"}
-          />
+        <HornedBeast
+          title="UniWhal"
+          description="A unicorn and a narwhal nuzzling their horns"
+          imageUrl="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
+        />
+        <HornedBeast
+          title="Rhino Family"
+          description="Parent rhino with two babies"
+          imageUrl="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
+        />
       </>
     );
   } 
